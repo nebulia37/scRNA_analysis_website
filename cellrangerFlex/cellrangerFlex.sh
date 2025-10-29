5731f28eebe0c6229e0a6a1f835cec35e5fa750f
Shunian Xiang <shunian.xiang@admerahealth.com>
2025-10-29 15:53:29 -0400

    refine and update cellranger flex pipeline

--- cellrangerFlex/cellrangerFlex.sh
+++ cellrangerFlex/cellrangerFlex.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
 
-workdir="/mnt/novaoutput/shunian/scRNA/cellrangerOnly/25300-01"
-projectID="25300-01"
-outputdir="$workdir/toDeliver/cellrangerResult"
-fastqdir="/mnt/novaoutput/Delivery_Team/1-Analysis/25300-01_S1-2025-10-16-09/SampleSheet-101325-891-XB-fcB--10nt-10nt-1-mismatch-2025-10-16-09"
-cellranger="/mnt/novaoutput/shunian/pipelines/scRNA/tools/cellranger-9.0.1/cellranger"
-species="mouse"
-sampleInfo_file="$workdir/script/sampleInfo.csv" ## sample_id,probe_barcode_ids,description| sample1,BC001,Control| sample2,BC003,Treated
-name="25300XC-01"
+parameter_file=$1
+readarray -t params < <(awk -F',' '{print $2}' "$parameter_file")
+
+workdir="${params[0]}"
+poolID="${params[1]}"
+fastqdir="${params[2]}"
+species="${params[3]}"
+sampleInfoFile="${params[4]}"
+fastq_id="${params[5]}"
+cellranger="${params[6]}"
+
+outputdir=$workdir/toDeliver/cellrangerResult/$poolID
+mkdir -p $outputdir
+sampleInfo_file=$workdir/script/$sampleInfoFile
 
 if [ $species = "mouse" ]
 then
@@ -20,18 +27,18 @@ then
     probeSet="/mnt/novaoutput/shunian/pipelines/scRNA/tools/cellranger-9.0.1/probe_sets/Chromium_Human_Transcriptome_Probe_Set_v1.1.0_GRCh38-2024-A.csv"
 fi
 
-touch $workdir/script/${projectID}_config.csv
-echo "[gene-expression]" >$workdir/script/${projectID}_config.csv
-echo "reference,$refGenome" >>$workdir/script/${projectID}_config.csv
-echo "probe-set,$probeSet" >>$workdir/script/${projectID}_config.csv
-echo "create-bam,true" >>$workdir/script/${projectID}_config.csv
-echo " " >>$workdir/script/${projectID}_config.csv
-echo "[libraries]" >>$workdir/script/${projectID}_config.csv
-echo "fastq_id,fastqs,feature_types" >>$workdir/script/${projectID}_config.csv
-echo "$name,$fastqdir,Gene Expression" >>$workdir/script/${projectID}_config.csv
-echo " " >>$workdir/script/${projectID}_config.csv
-echo "[samples]" >>$workdir/script/${projectID}_config.csv
-cat $sampleInfo_file >>$workdir/script/${projectID}_config.csv
+touch $workdir/script/${poolID}_config.csv
+echo "[gene-expression]" >$workdir/script/${poolID}_config.csv
+echo "reference,$refGenome" >>$workdir/script/${poolID}_config.csv
+echo "probe-set,$probeSet" >>$workdir/script/${poolID}_config.csv
+echo "create-bam,true" >>$workdir/script/${poolID}_config.csv
+echo " " >>$workdir/script/${poolID}_config.csv
+echo "[libraries]" >>$workdir/script/${poolID}_config.csv
+echo "fastq_id,fastqs,feature_types" >>$workdir/script/${poolID}_config.csv
+echo "$fastq_id,$fastqdir,Gene Expression" >>$workdir/script/${poolID}_config.csv
+echo " " >>$workdir/script/${poolID}_config.csv
+echo "[samples]" >>$workdir/script/${poolID}_config.csv
+cat $sampleInfo_file >>$workdir/script/${poolID}_config.csv
 
-$cellranger multi --id $name --csv $workdir/script/${projectID}_config.csv --output-dir $outputdir 
+$cellranger multi --id $fastq_id --csv $workdir/script/${poolID}_config.csv --output-dir $outputdir 
 
